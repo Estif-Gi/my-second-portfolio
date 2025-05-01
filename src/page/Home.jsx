d07b07dff6cf8ff01f1e793b6ce8fc85fc2795b2
Estifanos Girma <estifanosgirma4@gmail.com>
2025-05-02 00:10:11 +0300

    m

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return ( 
         <>
         <div className="sm:flex justify-self-center md:gap-24 lg:gap-72 p-4">
-            <img className="motion-preset-slide-right motion-duration-2000 w-52 rounded-full sm:w-96 ml-4 flex justify-self-center" src="\images\profile-2.jpg" alt=""/>
+            <img className="motion-preset-slide-right motion-duration-2000 w-52 rounded-full sm:w-96 ml-4 flex justify-self-center" src="\images\profile-2.jpg" alt="profile"/>
             <p className="pb-5 motion-preset-slide-left motion-duration-2000 text-lime-400  dark:text-tx text-3xl mt-4 sm:text-5xl font-bold text-center flex self-end flex-col ">
                 Hi i’m <br/>
                 <span className=" text-gray-500 dark:text-white p-2">ESTIFANOS GIRMA</span>
